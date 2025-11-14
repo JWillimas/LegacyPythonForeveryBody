7f65bdd57ef2f4bef8faaff9e930d1c6166f5933
JWillimas <1989687823@qq.com>
2025-11-14 10:16:04 -0600

    25.11.14

---
+++ PythonBasics/Introduction to Python.py
@@ -0,0 +1,53 @@
+#Escape the single or double quotation mark---------------->
+#in the string with a backslash (\). 
+# With this method
+#  you can use either single or double quotation marks to 
+# wrap the string itself:
+quote="She said ,\"hellow\""
+print(quote)
+
+#MultiLine---------------->
+my_str_3="""Multiline
+string"""
+print(my_str_3)
+
+my_str_4="""
+Another
+multiline
+string
+"""
+print(my_str_4)
+
+
+#"[]"square brackets---------------->
+#Work with string---------------->
+
+my_str=' hellow wrold '
+trimed_my_str=my_str.strip()
+print(trimed_my_str)
+
+Switched_my_str=my_str.replace('hellow','hi')
+print(Switched_my_str)
+
+my_str = 'hello world'
+
+split_words = my_str.split()
+print(type(split_words))
+
+find_str=my_str.find('o')
+print(find_str)
+
+#Floor division operator'//(forward slash)'---cpar---modulo operator%(reminder)
+
+
+#bitwise operator:
+greet = 2 #010
+greet &= 3 #011
+print(greet)    # 010&011=010(binary)=2(dec)
+print(f'\n')    # 010&011=010(binary)=2(dec)
+
+
+#>>This shifts the bits to the right by n positions.
+a=8 #1000
+a>>=2 #0010
+print(a)
